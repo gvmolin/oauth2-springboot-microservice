ba7a9a3fb2c7fee9df4486017aa6aaa754eef801
Gabriel Molin <molinvini@gmail.com>
2025-05-28 13:40:12 -0300

    minor changes

--- src/main/java/com/oauth2proj/utils/dto/ResponseDTO.java
+++ src/main/java/com/oauth2proj/utils/dto/ResponseDTO.java
@@ -1,8 +1,13 @@
 package com.oauth2proj.utils.dto;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class ResponseDTO<T> {
+
     public boolean success;
     public int code;
     public T data;
+    public String message;
     
 }
