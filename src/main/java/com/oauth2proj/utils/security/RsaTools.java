a97ab4e5c667b66a7e0155c96eb0ef508f20148c
Gabriel Molin <molinvini@gmail.com>
2025-05-28 14:08:15 -0300

    feat: create keys folder if it doesnt exists

--- src/main/java/com/oauth2proj/utils/security/RsaTools.java
+++ src/main/java/com/oauth2proj/utils/security/RsaTools.java
@@ -3,6 +3,8 @@ package com.oauth2proj.utils.security;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
@@ -19,25 +21,36 @@ public record RsaTools() {
             String privateKey = Base64.getEncoder().encodeToString(pair.getPrivate().getEncoded());
 
             
-            this.writeKeyFile("private", privateKey);
-            this.writeKeyFile("public", publicKey);
+            this.manageKeyFile("private", privateKey);
+            this.manageKeyFile("public", publicKey);
             
     }
 
-    private void writeKeyFile(String type, String key) throws IOException{
+    private void manageKeyFile(String type, String key) throws IOException{
         String currentDir = System.getProperty("user.dir");
-        String fullPath = currentDir + "/keys/" + type.toLowerCase() + "-key.pem";
+        String keysPath = (currentDir + "/keys/").replace("/", "\\");
+        String fullPath = keysPath + type.toLowerCase() + "-key.pem";
         String parsedFullPath = fullPath.replace("/", "\\");
-        Boolean exists = new File(parsedFullPath).exists();
-        if(exists) return;
+
+        Boolean keysPathExists = new File(keysPath).exists();
+        if(!keysPathExists) {
+            Files.createDirectory(Paths.get(keysPath));
+        }
+
+        Boolean fullPathExists = new File(parsedFullPath).exists();
+        if(fullPathExists) return;
         
+        writeFile(key, fullPath, type);
+    }
+
+    private void writeFile(String key, String path, String type) throws IOException{
         String content = (
             "-----BEGIN " + type.toUpperCase() + " KEY-----\n" + 
             key +
             "\n-----END " + type.toUpperCase() + " KEY-----" 
         );
 
-        try (FileWriter fileWriter = new FileWriter(parsedFullPath, false)) {
+        try (FileWriter fileWriter = new FileWriter(path, false)) {
             fileWriter.write(content);
             fileWriter.flush();
             fileWriter.close();
